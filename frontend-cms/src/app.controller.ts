41d9d034dfaf6fbe478b9505f14e2472081ba792
NazmiMustafa <nazmi_mustafa@teknokrat.ac.id>
2026-04-06 10:36:26 +0700

    update cms

--- frontend-cms/src/app.controller.ts
+++ frontend-cms/src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import axios from 'axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getDashboard() {
+    return {
+      message: 'PoolTick CMS jalan bro 🔥'
+    };
+  }
+
+  @Get('tickets')
+  async getTickets() {
+    const res = await axios.get('http://localhost:3000/tickets');
+    return res.data;
   }
 }
