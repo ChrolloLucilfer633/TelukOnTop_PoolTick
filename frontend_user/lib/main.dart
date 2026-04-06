78c167cc00e77db6ffe0619f23758af38407ae17
NazmiMustafa <nazmi_mustafa@teknokrat.ac.id>
2026-04-06 10:15:21 +0700

    flutter update

--- frontend_user/lib/main.dart
+++ frontend_user/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -8,28 +9,36 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  Future<String> getData() async {
+  Future<List> getTickets() async {
     final res = await http.get(
-      Uri.parse("http://10.0.2.2:3000"),
+      Uri.parse("http://localhost:3000/tickets"),
     );
-    return res.body;
+    return jsonDecode(res.body);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("Test API")),
+        appBar: AppBar(title: const Text("PoolTick")),
         body: FutureBuilder(
-          future: getData(),
+          future: getTickets(),
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
+            if (!snapshot.hasData) {
               return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasData) {
-              return Center(child: Text(snapshot.data.toString()));
-            } else {
-              return const Center(child: Text("Error"));
             }
+
+            var data = snapshot.data as List;
+
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  title: Text(data[i]['name']),
+                  subtitle: Text("Rp ${data[i]['price']}"),
+                );
+              },
+            );
           },
         ),
       ),
