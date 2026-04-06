f3ce89739d9167d4ced4d4b1975d3446d1a780f8
ChrolloLucilfer633 <kolonelgallih4@gmail.com>
2026-04-06 09:56:54 +0700

    git add .git add .rontend-cms/tsconfig.js

---
+++ Backend/src/tickets/tickets.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TicketsService {
+  private tickets = [
+    { id: 1, name: 'Tiket Dewasa', price: 20000 },
+    { id: 2, name: 'Tiket Anak', price: 10000 },
+  ];
+
+  findAll() {
+    return this.tickets;
+  }
+}
