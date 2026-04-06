37f0e6ffc734333f42983547fa54905364ad5d1f
NazmiMustafa <nazmi_mustafa@teknokrat.ac.id>
2026-04-06 10:56:52 +0700

    update cms

--- frontend-cms/src/app.controller.ts
+++ frontend-cms/src/app.controller.ts
@@ -4,13 +4,6 @@ import axios from 'axios';
 @Controller()
 export class AppController {
 
-  @Get()
-  getDashboard() {
-    return {
-      message: 'PoolTick CMS jalan bro 🔥'
-    };
-  }
-
   @Get('tickets')
   async getTickets() {
     const res = await axios.get('http://localhost:3000/tickets');
--- frontend-cms/src/main.ts
+++ frontend-cms/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.use(express.static(join(__dirname, '..', 'src', 'views')));
+
+  await app.listen(3001);
 }
 bootstrap();
