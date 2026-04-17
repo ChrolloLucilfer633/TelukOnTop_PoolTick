22588f44973656a4be4be754bd90d73de79fd3d3
NazmiMustafa <nazmi_mustafa@teknokrat.ac.id>
2026-04-17 09:39:21 +0700

    update cms nama pembeli

--- Backend/src/transactions/transactions.service.ts
+++ Backend/src/transactions/transactions.service.ts
@@ -15,27 +15,27 @@ export class TransactionsService {
   }
 
   // CREATE TRANSAKSI
-      async create(data: any) {
-      console.log("DATA MASUK:", data);
+  async create(data: any) {
+    console.log("DATA MASUK:", data);
 
-      const ticketId = Number(data.ticketId);
+    const ticketId = Number(data.ticketId);
 
-      const ticket = await this.prisma.ticket.findUnique({
-        where: { id: ticketId },
-      });
+    const ticket = await this.prisma.ticket.findUnique({
+      where: { id: ticketId },
+    });
 
-      if (!ticket) {
-        return { message: 'Ticket tidak ditemukan' };
-      }
-
-      return this.prisma.transaction.create({
-        data: {
-          name: data.name,     
-          price: ticket.price,
-          ticketId: ticket.id,
-        },
-      });
+    if (!ticket) {
+      return { message: 'Ticket tidak ditemukan' };
     }
+
+    return this.prisma.transaction.create({
+      data: {
+        name: data.name,
+        price: ticket.price,
+        ticketId: ticket.id,
+      },
+    });
+  }
 
   // DELETE
   delete(id: number) {
